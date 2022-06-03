2adc837929fa6eed4e60bd3d9872c2fa500969c1
Renugasekar <102687350+Renugasekar@users.noreply.github.com>
2022-06-03 20:12:45 +0530

    Update bloc_post.dart

--- lib/bloc_post.dart
+++ lib/bloc_post.dart
@@ -1,9 +1,9 @@
 import 'package:intl/intl.dart';
 
-class BlocPost{
+class BlogPost{
   final String title;
   final DateTime publishedDate;
   final String data;
   String get date=>DateFormat('MMMM d y').format(publishedDate);
-  BlocPost({required this.title,required this.data, required this.publishedDate});
+  BlogPost({required this.title,required this.data, required this.publishedDate});
 }
